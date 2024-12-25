c66334d20119dcfc7a91da873735f497c8cff460
Eyuel Dejene <dejenejoel@gmail.com>
2024-12-25 23:51:05 +0300

    role middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import errorHandler from "./middlewares/error_handler";
 import router from "./routes";
+import verifyAuth from "./middlewares/verify_auth";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -11,7 +12,7 @@ const app = express();
 app.use(morgan("tiny"));
 app.use(express.urlencoded({extended: true}));
 
-app.use('/api', router);
+app.use('/api', verifyAuth, router);
 
 app.use(errorHandler);
 app.listen(port, () => {
--- src/middlewares/verify_auth.ts
+++ src/middlewares/verify_auth.ts
@@ -1,7 +1,7 @@
 import {Request, Response, NextFunction} from 'express';
 import { DocumentT, fql } from 'fauna';
 import faunaClient from '../config/fauna_client';
-import { FaunaSession } from '../models/models';
+import { FaunaSession, User } from '../models/models';
 
 const verifyAuth = async (req: Request, res: Response, next: NextFunction) =>{
     try {
@@ -16,9 +16,10 @@ const verifyAuth = async (req: Request, res: Response, next: NextFunction) =>{
         const {data: session} = await faunaClient.query<DocumentT<FaunaSession>>(
             fql `let session = FaunaSession.bySessionId(${session_id ?? "234523452345345"})!
             ${sessionProjection}`
-        ); 
-    
+        );
+
         console.log(session);
+
         res.end();
     } catch (error) {
         next(error);
