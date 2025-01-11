b2bea82a679c7957c6ad290c9cc12c21ce40e418
Eyuel Dejene <dejenejoel@gmail.com>
2025-01-11 23:50:54 +0300

    small changes

--- src/config/fauna_client.ts
+++ src/config/fauna_client.ts
@@ -2,9 +2,6 @@ import {Client} from 'fauna';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-const faunaClient = new Client(
-    {secret: process.env.FAUNA_SERVER_KEY || ""}
-);
+const faunaClient = new Client({secret: process.env.FAUNA_SERVER_KEY || ""});
 
 export default faunaClient;
--- src/config/roles.ts
+++ src/config/roles.ts
@@ -1,7 +1,7 @@
 const roles = {
-    ADMIN : 3000,
-    EDITOR : 2000,
-    USER : 1000
+    ADMIN: 3000,
+    EDITOR: 2000,
+    USER: 1000
 };
 
 export default roles;
